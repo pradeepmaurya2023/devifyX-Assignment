0ac09f65ac20451de4df73cf564bc59666256795
Pradeep <pradeepgsv51@gmail.com>
2025-06-18 18:57:04 +0530

    Added files schema

---
+++ models/File.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+  originalName: {
+    type: String,
+    required: true,
+  },
+  storedName: {
+    type: String,
+    required: true,
+  },
+  mimeType: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number, // in bytes
+    required: true,
+    min: 0,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+  isPublic: {
+    type: Boolean,
+    default: false,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now,
+  },
+  uploader: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+});
+
+const File = mongoose.model("File", fileSchema);
+
+module.exports = File;
